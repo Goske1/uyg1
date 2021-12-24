c27a3a36995068c1c3e77bc3cd2401e6b36dfc0c
GOKSE1 <gokselbekdemir431@gmail.com>
2021-12-24 11:20:10 +0300

    AH

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+void main()=>runApp(ahmet());
 class ahmet extends StatelessWidget {
   const ahmet({Key? key}) : super(key: key);
 
@@ -7,27 +7,79 @@ class ahmet extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+
       title: 'flutter ilk demo',
       theme: ThemeData(
-
       ),
-    );
-  }
-}
+      home: Scaffold(
 
-class myhome extends StatelessWidget {
-  const myhome({Key? key}) : super(key: key);
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(60.0),
+            child: AppBar(
+              backgroundColor: Colors.black87,
+              title: Text("Günaydın"),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black87,
-      appBar: AppBar(
-        title: CircleAvatar(
-          radius: 40,
+              actions: [
+
+                IconButton(onPressed: (){}, icon: Icon(Icons.flash_on)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.history)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.article)),
+                IconButton(onPressed: (){}, icon: Icon(Icons.settings)),
+              ],
+            ),
+          ),
+
+
+
+
+
+
+
+
+
+          bottomNavigationBar: BottomAppBar(
+
+          shape: CircularNotchedRectangle(),
+      child: Container(
+        height: 75,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              iconSize: 30.0,
+              icon: Icon(Icons.home),
+              onPressed: () {
+
+              },
+            ),
+            IconButton(
+              iconSize: 30.0,
+              icon: Icon(Icons.search),
+              onPressed: () {
+
+
+
+              },
+            ),
+
+            IconButton(
+              iconSize: 30.0,
+
+              icon: Icon(Icons.list),
+              onPressed: () {
+
+              },
+            )
+          ],
         ),
       ),
+    ),
+    ),
     );
+
+
+
   }
 }
 
